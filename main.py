ac9d23ef0f528dc657890998ea4c47186fe847c7
Sven <Sven_Kuehne@hotmail.de>
2022-02-09 01:20:46 +0000

    1st look

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ from pytest import main
 from arithmetic_arranger import arithmetic_arranger
 
 
-print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
+print(arithmetic_arranger(["32 + 698"]))  # 1st plus
+# print(arithmetic_arranger(["3801 - 2"]))  # 2nd minus
+# print(arithmetic_arranger(["32 + 698", "3801 - 2"]))  #3rd both
+# print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])) # 4th all
 
 
 # Run unit tests automatically
