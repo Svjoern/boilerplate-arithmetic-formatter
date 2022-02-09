d5e87b253b7a2d2541605b8cefc55721479259c3
Sven <Sven_Kuehne@hotmail.de>
2022-02-09 02:14:19 +0000

    ...

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,4 +1,47 @@
-def arithmetic_arranger(problems):
+def arithmetic_arranger(problems, show_answer=False):
+  # problems_split = []
+  spacing = get_spacing(problems)
+  top     = ""
+  mid     = ""
+  line    = ""
+  res     = ""
 
+  for elem in problems:
+    arry = split_elements(elem,show_answer,True)
 
-    return arranged_problems
+    if len(arry[0]) >= len(arry[2]):
+      length = len(arry[0]) + 2
+    else:
+      length = len(arry[2]) + 2
+    # problems_split.append(arry)
+
+  for i in range(length-arry[0]):
+    pass
+
+  if show_answer:
+    return top + mid + line + res
+  else:
+    return top + mid + line
+
+def split_elements(element, show_answer=False, PrintArr=False):
+  arry = element.split(" ")
+  if show_answer:
+    arry.append(" " + str(get_result(arry)))
+  if PrintArr:
+    print(arry)
+  return arry
+
+def get_result(array):
+  a = int(array[0]) 
+  b = int(array[2])
+  op = array[1]
+  if op == "+":
+    return a+b
+  if op == "-":
+    return a-b
+
+def get_spacing(problems):
+  if len(problems) != 1:
+    return "   "
+  else:
+    return ""
--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from pytest import main
 
 from arithmetic_arranger import arithmetic_arranger
 
-
-print(arithmetic_arranger(["32 + 698"]))  # 1st plus
+(arithmetic_arranger(["32 + 698"],True))
+(arithmetic_arranger(["32 + 698"]))
+# print(arithmetic_arranger(["32 + 698"]))  # 1st plus
 # print(arithmetic_arranger(["3801 - 2"]))  # 2nd minus
 # print(arithmetic_arranger(["32 + 698", "3801 - 2"]))  #3rd both
 # print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])) # 4th all
 
 
 # Run unit tests automatically
-main()
+# main()
